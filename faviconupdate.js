f35c380a7ed4f43c0aa78ab436f041cc70a580d7
SnideB0t <snideb0t@techachance.com>
2022-03-15 09:09:29 +0100

    Update icons to latest version + add missing services

--- faviconupdate.js
+++ faviconupdate.js
@@ -1,110 +1,222 @@
 // Define all the AWS services here
 const SERVICES = [
-	//"a4b", // Amazon did this one!
-	"acm",
-	"amazon-mq",
-	"apigateway",
-	"appsync",
-	"artifact",
-	"athena",
-	"awsautoscaling",
-	"batch",
-	"billing",
-	//"budgets", // TODO: This has a special URL (console.aws.amazon.com/billing/home?#/budgets)
-	"cloud9",
-	"cloudformation",
-	"cloudfront",
-	"cloudhsm",
-	"cloudsearch",
-	"cloudtrail",
-	"cloudwatch",
-	"codebuild",
-	"codecommit",
-	"codedeploy",
-	"codepipeline",
-	"codestar",
-	"comprehend",
-	//"cognito", // Amazon did this one!
-	"config",
-	"connect",
-	"console",
-	"cost-reports",
-	"datapipeline",
-	"deeplens",
-	"directconnect",
-	//"directoryservice", // Amazon did this one!
-	"devicefarm",
-	"discovery",
-	"dms",
-	"dynamodb",
-	//"ec2",
-	"ec2sp",
-	"esv3",
-	"efs",
-	"ecs",
-	"ecr",
-	"eks",
-	"elasticache",
-	"elastictranscoder",
-	"elasticbeanstalk",
-	"elasticmapreduce",
-	"glacier",
-	"gamelift",
-	"glue",
-	//"greengrass", // unique URL
-	"guardduty",
-	//"iam", // Amazon did this one!
-	"iot",
-	"iotsitewise",
-	"importexport",
-	"inspector",
-	"kinesis",
-	"kinesisvideo",
-	"kms",
-	//"lambda", // Amazon did this one!
-	"lex",
-	"ls",
-	//"macie", // unique URL
-	"machinelearning",
-	"managed-services",
-	"mediaconvert",
-	"medialive",
-	"mediapackage",
-	"mediastore",
-	"mediatailor",
-	"migrationhub",
-	//"mobilehub",
-	"neptune",
-	"opsworks",
-	"pinpoint",
-	"polly",
-	"rds",
-	"redshift",
-	"rekognition",
-	"route53",
-	"s3",
-	"sagemaker",
-	//"secretsmanager", // Amazon did this one!
-	"servermigration",
-	"servicecatalog",
-	"ses",
-	"sns",
-	//"singlesignon", // Amazon did this one!
-	"sqs",
-	"states",
-	"storagegateway",
-	"sumerian",
-	"systems-manager",
-	"translate",
-	"transcribe",
-	"trustedadvisor",
-	"swf",
-	"waf",
-	"wafv2",
-	"workspaces",
-	"workmail",
-	//"vpc",
-	"xray"
+  //"budgets", // TODO: This has a special URL (console.aws.amazon.com/billing/home?#/budgets)
+  //"directoryservice", // Amazon did this one!
+  //"ec2",
+  //"greengrass", // unique URL
+  //"iam", // Amazon did this one!
+  //"lambda", // Amazon did this one!
+  //"macie", // unique URL
+  //"mobilehub",
+  //"secretsmanager", // Amazon did this one!
+  //"singlesignon", // Amazon did this one!
+  //"vpc",
+  "a2i",
+  "a4b",
+  "acm",
+  "amazon-mq",
+  "amplify",
+  // "apigateway", // Amazon did this one!
+  "app2container",
+  "appflow",
+  "appmesh",
+  "apprunner",
+  "appstream2",
+  "appsync",
+  "artifact",
+  "athena",
+  "auditmanager",
+  "augmented-ai",
+  "aurora",
+  "awsautoscaling",
+  "backup",
+  "batch",
+  "billing",
+  "braket",
+  "chatbot",
+  "chime",
+  "cloud9",
+  "cloudformation",
+  "cloudfront",
+  "cloudhsm",
+  "cloudmap",
+  "cloudsearch",
+  // "cloudshell", // Amazon did this one!
+  "cloudtrail",
+  // "cloudwatch", // Amazon did this one!
+  "codeartifact",
+  "codebuild",
+  "codecommit",
+  "codedeploy",
+  "codeguru",
+  "codepipeline",
+  "codestar",
+  "comprehend",
+  "config",
+  "connect",
+  "console",
+  "containers",
+  "controltower",
+  "corretto",
+  "cost-management",
+  "cost-reports",
+  // "dataexchange", // Amazon did this one!
+  "datapipeline",
+  "datasync",
+  "deepcomposer",
+  "deeplens",
+  "deepracer",
+  "detective",
+  "devicefarm",
+  "devops-guru",
+  "directconnect",
+  "directoryservice",
+  "discovery",
+  "dms",
+  "documentdb",
+  "drs",
+  // "dynamodb", // Amazon did this one!
+  //"ec2",
+  "ec2sp",
+  "ecr",
+  "ecs",
+  "efs",
+  "eks",
+  "elasticache",
+  "elasticbeanstalk",
+  "elasticmapreduce",
+  "elastictranscoder",
+  "elb",
+  "elemental-appliances-software",
+  "emr",
+  "esv3",
+  "eventbridge",
+  "fargate",
+  "finspace",
+  "firewallmanager",
+  "fis",
+  "fmsv2",
+  "forecast",
+  "frauddetector",
+  "fsx",
+  "gamelift",
+  "glacier",
+  "globalaccelerator",
+  "glue",
+  "grafana",
+  "greengrass",
+  "groundstation",
+  "guardduty",
+  "healthlake",
+  "iam",
+  "inspector",
+  "iot",
+  "iot1click",
+  "iotanalytics",
+  "iotevents",
+  "iotfleetwise",
+  "iotroborunner",
+  "iotsitewise",
+  "iottwinmaker",
+  "ivs",
+  "kendra",
+  "keyspaces",
+  "kinesis",
+  "kinesisdata",
+  "kinesisfirehose",
+  "kinesisvideo",
+  "kms",
+  "lake-formation",
+  "lambda",
+  "launchwizard",
+  "lex",
+  "license-manager",
+  "lightsail",
+  // "location", // Amazon did this one!
+  "lookoutequipment",
+  "lookoutmetrics",
+  "lookoutvision",
+  // "ls", // Amazon did this one!
+  "m2",
+  "machinelearning",
+  "macie",
+  "managed-blockchain",
+  "managed-services",
+  "marketplace",
+  "mediaconnect",
+  "mediaconvert",
+  "medialive",
+  "mediapackage",
+  "mediastore",
+  "mediatailor",
+  "memorydb",
+  "mgn",
+  "migrationhub",
+  "monitron",
+  "msk",
+  "neptune",
+  "nimblestudio",
+  "opensearch-service",
+  "opsworks",
+  "organizations",
+  "outpost",
+  "panorama",
+  "personalize",
+  "phd",
+  "pinpoint",
+  "polly",
+  "prometheus",
+  "proton",
+  "qldb",
+  "quicksight",
+  "ram",
+  "rds",
+  "redshift",
+  "rekognition",
+  "resiliencehub",
+  "robomaker",
+  "rosa",
+  "route53",
+  "s3",
+  "sagemaker",
+  "secretsmanager",
+  "securityhub",
+  "serverless",
+  "servermigration",
+  "servicecatalog",
+  "ses",
+  "shield",
+  "shieldv2",
+  "signer",
+  "singlesignon",
+  "snow",
+  "sns",
+  "sqs",
+  "sso",
+  "states",
+  "step-functions",
+  "storagegateway",
+  "sumerian",
+  "systems-manager",
+  "textract",
+  "thingsgraph",
+  "timestream",
+  "transcribe",
+  "transfer",
+  "translate",
+  "trustedadvisor",
+  "vmware",
+  //"vpc",
+  "waftool",
+  "wafv2",
+  "wavelength",
+  "wellarchitected",
+  "workdocs",
+  "worklink",
+  "workmail",
+  "workspaces",
+  "xray",
+
 ];
 
 // Look for the string blocks right after the 'amazon.com/' (ec2/s3/iam/ses/etc...)
@@ -113,127 +225,119 @@ let reg_ec2_and_vpc = /:\/\/([a-z0-9.-]*)\/([a-z0-9.-]*)\/([a-zA-Z0-9\.\-\#\:\?\
 let reg_full = /:\/\/([a-z0-9.-]*)\/([a-z0-9.-]*)\/([a-z0-9.-]*)\/([a-zA-Z0-9\.\-\#\:\?\=]*)*/g;
 
 // Break the URL into parts and capture the string after the 'amazon.com/' as awsServiceName
-let captureGroupArray = Array.from( document.URL.matchAll(reg) );
+let captureGroupArray = Array.from(document.URL.matchAll(reg));
 // Note:  This is just to keep my sanity in check.  Most URLs follow the format at the top.  These follow others.
-let captureGroupArrayEC2AndVPC = Array.from( document.URL.matchAll(reg_ec2_and_vpc) );
-let captureGroupArrayLong = Array.from( document.URL.matchAll(reg_full) );
+let captureGroupArrayEC2AndVPC = Array.from(document.URL.matchAll(reg_ec2_and_vpc));
+let captureGroupArrayLong = Array.from(document.URL.matchAll(reg_full));
 
 let domain = captureGroupArray[0][1];
 
 if (domain !== "docs.aws.amazon.com") {
-	let awsServiceName = captureGroupArray[0][2];
+  let awsServiceName = captureGroupArray[0][2];
+  let subAWSServiceName = captureGroupArray[0][3];
 
-	// For Codesuite URLs (Codebuild/CodeDeploy/CodePipeline/etc...), we need to break the URL apart further to work
-	if (awsServiceName == 'codesuite') {
-		awsServiceName = captureGroupArray[0][3];
-	}
+  // For Codesuite URLs (Codebuild/CodeDeploy/CodePipeline/etc...), we need to break the URL apart further to work
+  if (awsServiceName == 'codesuite') {
+    awsServiceName = subAWSServiceName
+  }
+  if (awsServiceName == 'wafv2' && subAWSServiceName !== 'homev2') {
+    awsServiceName = subAWSServiceName
+  }
 
-    let awsService = awsServiceName + ".png";
-	// We have found a match in the URL!
-	    if (SERVICES.includes(awsServiceName)) {
-			// Build the icon and shortcut icon tags so we can add them inside the head tag
-			let iconNode = document.createElement('link');
-			iconNode.setAttribute('rel', 'icon');
-			iconNode.setAttribute('type', 'image/png');
-			iconNode.setAttribute('href', chrome.runtime.getURL(`icons/${awsService}`));
-			let shortcutIconNode = document.createElement('link');
-			shortcutIconNode.setAttribute('rel', 'shortcut icon');
-			shortcutIconNode.setAttribute('type', 'image/png');
-			shortcutIconNode.setAttribute('href', chrome.runtime.getURL(`icons/${awsService}`));
+  let awsService = awsServiceName + ".png";
+  let iconName = ''
+  // We have found a match in the URL!
+  if (SERVICES.includes(awsServiceName)) {
 
-			// Add the tags we just made to the head tag
-			document.getElementsByTagName('head')[0].appendChild(iconNode);
-			document.getElementsByTagName('head')[0].appendChild(shortcutIconNode);
-		} else if(awsServiceName === 'ec2') {
+    iconName = awsService
 
-			// There is a "v2" url for a bunch of these, and it flips on and off for no reason.  Check for this.
+  } else if (awsServiceName === 'ec2') {
+    // There is a "v2" url for a bunch of these, and it flips on and off for no reason.  Check for this.
+    let endOfURL = ""
+    let prefix = "ec2_"
 
-			let endOfURL = ""
+    if (captureGroupArray[0][3] == "v2") {
+      endOfURL = captureGroupArrayLong[0][4]
+    } else {
+      endOfURL = captureGroupArrayEC2AndVPC[0][3]
+    }
 
-			if (captureGroupArray[0][3] == "v2") {
-				endOfURL = captureGroupArrayLong[0][4]
-			} else {
-				endOfURL = captureGroupArrayEC2AndVPC[0][3]
-			}
+    if (endOfURL.includes('AutoScalingGroups')) {
+      awsServiceName = 'AutoScalingGroups';
+    } else if (endOfURL.includes('Addresses')) {
+      awsServiceName = 'Addresses';
+    } else if (endOfURL.includes('LoadBalancers')) {
+      awsServiceName = 'LoadBalancers';
+    } else if (endOfURL.includes('Images')) {
+      awsServiceName = 'Images';
+    } else if (endOfURL.includes('Instances')) {
+      awsServiceName = 'Instances';
+    } else if (endOfURL.includes('NIC')) {
+      awsServiceName = 'NIC';
+    } else {
+      awsServiceName = 'ec2';
+      prefix = ''
+    }
 
-			if (endOfURL.includes('AutoScalingGroups')) {
-				awsServiceName = 'AutoScalingGroups';
-			} else if (endOfURL.includes('Addresses')) {
-				awsServiceName = 'Addresses';
-			} else if (endOfURL.includes('LoadBalancers')) {
-				awsServiceName = 'LoadBalancers';
-			} else if (endOfURL.includes('Images')) {
-				awsServiceName = 'Images';
-			} else if (endOfURL.includes('NIC')) {
-				awsServiceName = 'NIC';
-			}
+    iconName = prefix + awsServiceName + ".png";
 
-			// TODO: Make all this code a function
-			let awsService = awsServiceName + ".png";
+  } else if (awsServiceName === 'vpc') {
 
-			let iconNode = document.createElement('link');
-			iconNode.setAttribute('rel', 'icon');
-			iconNode.setAttribute('type', 'image/png');
-			iconNode.setAttribute('href', chrome.runtime.getURL(`icons/${awsService}`));
-			let shortcutIconNode = document.createElement('link');
-			shortcutIconNode.setAttribute('rel', 'shortcut icon');
-			shortcutIconNode.setAttribute('type', 'image/png');
-			shortcutIconNode.setAttribute('href', chrome.runtime.getURL(`icons/${awsService}`));
+    let awsServiceName = captureGroupArrayEC2AndVPC[0][3];
+    let prefix = "vpc_"
 
-			document.getElementsByTagName('head')[0].appendChild(iconNode);
-			document.getElementsByTagName('head')[0].appendChild(shortcutIconNode);
+    if (awsServiceName.includes('RouteTables')) {
+      awsServiceName = 'RouteTables';
+    } else if (awsServiceName.includes('igws')) {
+      awsServiceName = 'igws';
+    } else if (awsServiceName.includes('Addresses')) {
+      awsServiceName = 'Addresses';
+    } else if (awsServiceName.includes('Endpoints')) {
+      awsServiceName = 'Endpoints';
+    } else if (awsServiceName.includes('NatGateways')) {
+      awsServiceName = 'NatGateways';
+    } else if (awsServiceName.includes('PeeringConnections')) {
+      awsServiceName = 'PeeringConnections';
+    } else if (awsServiceName.includes('acls')) {
+      awsServiceName = 'acls';
+    } else if (awsServiceName.includes('CustomerGateways')) {
+      awsServiceName = 'CustomerGateways';
+    } else {
+      awsServiceName = 'vpc';
+      prefix = ''
+    }
 
-		} else if(awsServiceName === 'vpc') {
+    iconName = prefix + awsServiceName + ".png";
 
-			let awsServiceName = captureGroupArrayEC2AndVPC[0][3];
+  }
 
-			if (awsServiceName.includes('RouteTables')) {
-				awsServiceName = 'RouteTables';
-			} else if (awsServiceName.includes('igws')) {
-				awsServiceName = 'igws';
-			} else if (awsServiceName.includes('Addresses')) {
-				awsServiceName = 'Addresses';
-			} else if (awsServiceName.includes('Endpoints')) {
-				awsServiceName = 'Endpoints';
-			} else if (awsServiceName.includes('NatGateways')) {
-				awsServiceName = 'NatGateways';
-			} else if (awsServiceName.includes('PeeringConnections')) {
-				awsServiceName = 'PeeringConnections';
-			} else if (awsServiceName.includes('acls')) {
-				awsServiceName = 'acls';
-			} else if (awsServiceName.includes('CustomerGateways')) {
-				awsServiceName = 'CustomerGateways';
-			}	else {
-				awsServiceName = 'vpc';
-			}
+  if (iconName !== '') {
+    // TODO: Make all this code a function
+    let iconNode = document.createElement('link');
+    iconNode.setAttribute('rel', 'icon');
+    iconNode.setAttribute('type', 'image/png');
+    iconNode.setAttribute('href', chrome.runtime.getURL(`icons/${iconName}`));
+    let shortcutIconNode = document.createElement('link');
+    shortcutIconNode.setAttribute('rel', 'shortcut icon');
+    shortcutIconNode.setAttribute('type', 'image/png');
+    shortcutIconNode.setAttribute('href', chrome.runtime.getURL(`icons/${iconName}`));
 
-			let awsService = awsServiceName + ".png";
+    // Add the tags we just made to the head tag
+    document.getElementsByTagName('head')[0].appendChild(iconNode);
+    document.getElementsByTagName('head')[0].appendChild(shortcutIconNode);
+  }
+  // else {
+  //   // We have a favicon on the page, so just update the favicon tags
 
-			// TODO: Make all this code a function
-			let iconNode = document.createElement('link');
-			iconNode.setAttribute('rel', 'icon');
-			iconNode.setAttribute('type', 'image/png');
-			iconNode.setAttribute('href', chrome.runtime.getURL(`icons/${awsService}`));
-			let shortcutIconNode = document.createElement('link');
-			shortcutIconNode.setAttribute('rel', 'shortcut icon');
-			shortcutIconNode.setAttribute('type', 'image/png');
-			shortcutIconNode.setAttribute('href', chrome.runtime.getURL(`icons/${awsService}`));
+  //   // Get all the <link> tags
+  //   let linkElements = document.getElementsByTagName('link');
 
-			document.getElementsByTagName('head')[0].appendChild(iconNode);
-			document.getElementsByTagName('head')[0].appendChild(shortcutIconNode);
-
-		} else {
-			// We have a favicon on the page, so just update the favicon tags
-
-			// Get all the <link> tags
-			let linkElements = document.getElementsByTagName('link');
-
-			for (let i = 0; i < linkElements.length; i++) {
-				// There are 2 tags that control the favicon.  Update them to be the correct favicon
-				if (['icon', 'shortcut icon'].includes(linkElements[i].getAttribute('rel'))) {
-					linkElements[i].setAttribute('type', 'image/png');
-					linkElements[i].setAttribute('href', chrome.runtime.getURL(`icons/${awsService}`));
-				}
-			}
-		}
+  //   for (let i = 0; i < linkElements.length; i++) {
+  //     // There are 2 tags that control the favicon.  Update them to be the correct favicon
+  //     if (['icon', 'shortcut icon'].includes(linkElements[i].getAttribute('rel'))) {
+  //       linkElements[i].setAttribute('type', 'image/png');
+  //       linkElements[i].setAttribute('href', chrome.runtime.getURL(`icons/${awsService}`));
+  //     }
+  //   }
+  // }
 }
